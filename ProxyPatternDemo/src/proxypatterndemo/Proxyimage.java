a57ec79dfbf7a0d5d261af15417bc959f39c4d40
june <june@thamarat>
2017-02-20 01:23:14 +0700

    lab4

---
+++ ProxyPatternDemo/src/proxypatterndemo/Proxyimage.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proxypatterndemo;
+
+/**
+ *
+ * @author june
+ */
+public class Proxyimage {
+    public Realimage realimage;
+    public String fileNmae;
+    public Proxyimage(){}
+    public void display(){}
+    
+}
---
+++ ProxyPatternDemo/src/proxypatterndemo/Realimage.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proxypatterndemo;
+
+/**
+ *
+ * @author june
+ */
+public class Realimage implements image {
+    public String fileName;
+    public Realimage(){}
+    public void display(){}
+    public void loadFromDisk(){}
+}
