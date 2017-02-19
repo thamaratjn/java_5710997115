1a008c78c65da7c510fa42eaa6ea77a6b6d1807c
june <june@thamarat>
2017-02-20 01:23:50 +0700

    lab4

---
+++ SingletonPatternDemo/src/singletonpatterndemo/SingleObject.java
@@ -0,0 +1,17 @@
+/*/
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package singletonpatterndemo;
+
+/**
+ *
+ * @author june
+ */
+public class SingleObject {
+    private SingleObject instace;
+    private SingleObject(){}
+    public SingleObject getinstancce(){return null;}
+    public void showMessage(){}
+}
