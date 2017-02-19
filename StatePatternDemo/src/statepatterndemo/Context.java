01bca48e22fa9dc985737721495152fba8b8c2d7
june <june@thamarat>
2017-02-20 01:24:11 +0700

    lab4

---
+++ StatePatternDemo/src/statepatterndemo/Context.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statepatterndemo;
+
+/**
+ *
+ * @author june
+ */
+public class Context {
+    private State state;
+    public void Context (){}
+    public int gestate (){return 0;}
+    public void setState(){}
+}
